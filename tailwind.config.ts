afca7cf34a6e39ecbf230c9625050e4f9e115882
whevalmeze <princegbefavor@gmail.com>
2024-06-16 19:39:44 +0100

    fix: setup shadcn

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+import {fontFamily} from "tailwindcss/defaultTheme"
 import type { Config } from "tailwindcss"
 
 const config = {
@@ -71,6 +72,9 @@ const config = {
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+      },
+      fontFamily: {
+        sans: ["var(--font-sans)", ...fontFamily.sans],
       },
     },
   },
