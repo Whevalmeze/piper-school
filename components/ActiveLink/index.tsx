48375332fea78d12b59eb037b9d8f2483942a17c
whevalmeze <princegbefavor@gmail.com>
2024-06-22 02:42:03 +0100

    feat: add redirect to users page

--- components/ActiveLink/index.tsx
+++ components/ActiveLink/index.tsx
@@ -3,17 +3,19 @@ import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import { ReactNode } from 'react'
 
-
-const ActiveLink = (props: {link:string, icon: ReactNode}) => {
+const ActiveLink = (props: { link: string, icon: ReactNode }) => {
     const pathname = usePathname()
+    
+    const isActive = pathname === `/${props.link}` || pathname.startsWith(`/${props.link}/`);
+
     return (
-            <Link href={`/${props.link}`}>
-                <div className={`px-10 flex items-center gap-2 text-gray-300 capitalize py-3 rounded-xl ${"/" + props.link === pathname ? "bg-arapawa-500" : ""} `}>
-                    {props.icon}
-                    {props.link}
-                </div>
-            </Link>
-  )
+        <Link href={`/${props.link}`}>
+            <div className={`px-10 flex items-center gap-2 text-gray-300 capitalize py-3 rounded-xl ${isActive ? "bg-arapawa-500" : ""}`}>
+                {props.icon}
+                {props.link}
+            </div>
+        </Link>
+    )
 }
 
 export default ActiveLink
