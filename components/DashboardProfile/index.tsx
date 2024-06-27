f0f938dd96360d5d345a64af27ea473f7def095b
whevalmeze <princegbefavor@gmail.com>
2024-06-28 00:45:46 +0100

    fix: add key and use apos

--- components/DashboardProfile/index.tsx
+++ components/DashboardProfile/index.tsx
@@ -15,14 +15,14 @@ type Props = {
 
 const DashboardProfile = ({user}: Props) => {
   return (
-    <div className="flex flex-col lg:flex-row justify-between w-full">
+    <div className="relative flex flex-col lg:flex-row justify-between w-full">
         <div>
           <p className="text-xl">Hi, {user?.firstname}.</p>
           <h2 className="text-2xl font-bold">Welcome back, Admin.</h2>
         </div>
         <div className="flex items-center gap-2">
-          <Input placeholder="Search" /><Search />
-          <Avatar>
+          <div className="relative"><Input className="px-10" placeholder="Search" /><Search width={15} className="absolute top-[20%] left-3" /></div>
+          <Avatar className="absolute md:static top-0 right-0">
             <AvatarImage src=" " alt="@shadcn" />
             <AvatarFallback>{user.firstname[0]+user.lastname[0]}</AvatarFallback>
           </Avatar>
