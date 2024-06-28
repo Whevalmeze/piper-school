a92af68ad1c783089f37ab4128c1e1cfd336f25e
whevalmeze <princegbefavor@gmail.com>
2024-06-28 04:44:15 +0100

    feat: add image to hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 import Footer from "@/components/Footer"
 import Hero from "@/components/Hero"
 import SignInNav from "@/components/SignInNav"
+import Image from "next/image"
 
 const Home = () => {
   return (
     <main className="min-h-full flex flex-col">
         <SignInNav />
         <Hero />
+        <section className="w-full p-3 ">
+         <Image src="/das.png" className="border rounded-2xl m-auto" width={1280} height={840} alt="Piper School Demo Dashboard " />
+        </section> 
         <Footer />
     </main>
     )
--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -4,7 +4,7 @@ type Props = {}
 const Footer = (props: Props) => {
     const date: number = new Date().getFullYear()
   return (
-    <footer className='w-full p-5 fixed bottom-0'>
+    <footer className='w-full p-5 '>
         <div className='container m-auto text-center'>
             Copyright &copy; {date}.
         </div>
